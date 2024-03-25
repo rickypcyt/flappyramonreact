cfbddd23f40954a42340a3ceae78f253498250cd
rickypcyt <rickypcyt@gmail.com>
2024-03-25 14:02:21 +0100

    Anadi que no se pueda pausar mientras estas en game over

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import tubeImage from "./Images/pipes/pen2.png";
 import song from "./MALDITA_RAMONA_-_CHLY_feat._Montana_Recordzzz.mp3";
 import "./fonts.css";
 
-
-const gravity = -0.4; //Increased gravity 
-//const jumpStrength = 10; //Adjust jump strength for balance
+const gravity = -0.4;
 const tubeWidth = 52;
 const tubeHeight = 320;
 const tubeGap = 500;
 const tubeSpeed = 5;
+
 const generateRandomTubePosition = () => {
   const minY = window.innerHeight * -0.01;
   const maxY = window.innerHeight * -0.15;
   const randomY = Math.random() * (maxY - minY) + minY;
   return { x: window.innerWidth, yUpper: randomY, yLower: randomY - 10 };
 };
+
 const Tube = ({ tube }) => (
   <div className="tube" style={{ position: "absolute", left: tube.x, bottom: 0 }}>
     <img
@@ -45,6 +45,7 @@ const Tube = ({ tube }) => (
     />
   </div>
 );
+
 function App() {
   const [basePosition, setBasePosition] = useState(0);
   const [birdPosition, setBirdPosition] = useState(window.innerHeight / 2);
@@ -52,39 +53,45 @@ function App() {
   const [gameStarted, setGameStarted] = useState(false);
   const [gamePaused, setGamePaused] = useState(false);
   const [gameOver, setGameOver] = useState(false);
-  const [score, setScore] = useState(0)
+  const [score, setScore] = useState(0);
   const [tubes, setTubes] = useState([]);
-  const baseRef = useRef(null); 
+  const baseRef = useRef(null);
   const animateRef = useRef(null);
+  const audioRef = useRef(null);
+
   const handleKeyDown = useCallback((e) => {
     if (!gameStarted && e.keyCode === 32) {
       setGameStarted(true);
-      if(audioRef.current){
-        audioRef.current.play(); 
+      if (audioRef.current) {
+        audioRef.current.play();
       }
     } else if (gameStarted && !gamePaused && e.keyCode === 32) {
       setBirdVelocity(7);
-    } else if ((e.keyCode === 80 || e.keyCode === 27 || e.keyCode === 32) && gameStarted) {
+    } else if ((e.keyCode === 80 || e.keyCode === 27 || e.keyCode === 32) && gameStarted && !gameOver) {
       setGamePaused((prevPaused) => !prevPaused);
     }
-  }, [gameStarted, gamePaused]);
+  }, [gameStarted, gamePaused, gameOver]);
+
   const handleKeyPress = useCallback((e) => {
     if (e.keyCode === 32 && gameOver) {
       restartGame();
     }
   }, [gameOver]);
+
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
   }, [handleKeyDown]);
+
   useEffect(() => {
     document.addEventListener("keydown", handleKeyPress);
     return () => {
       document.removeEventListener("keydown", handleKeyPress);
     };
   }, [handleKeyPress]);
+
   const detectBaseCollision = useCallback(() => {
     const birdRect = document.querySelector(".bird").getBoundingClientRect();
     const baseRect = baseRef.current.getBoundingClientRect();
@@ -93,31 +100,25 @@ function App() {
       cancelAnimationFrame(animateRef.current);
     }
   }, []);
+
   useEffect(() => {
     const animate = () => {
       setBirdVelocity((prevVelocity) => prevVelocity + gravity);
       setBirdPosition((prevPosition) => prevPosition + birdVelocity);
       setBasePosition((prevPosition) => (prevPosition + 1) % (window.innerWidth + 100));
       setTubes((prevTubes) => {
-        let incrementScore = false; 
-        const newTubes = prevTubes
-        .map((tube) => {
+        let incrementScore = false;
+        const newTubes = prevTubes.map((tube) => {
           const newX = tube.x - tubeSpeed;
-    
-          // Check if a tube has just passed the bird's fixed x position to potentially increment the score
-          if (newX < 100 && tube.x >= 100) { // Assuming the bird's x position is around 100px
-            incrementScore = true; // Set flag to increment score
+          if (newX < 100 && tube.x >= 100) {
+            incrementScore = true;
           }
-    
-          return { ...tube, x: newX }; // Update tube position
-        })
-          
+          return { ...tube, x: newX };
+        }).filter((tube) => tube.x > -tubeWidth);
 
-          .filter((tube) => tube.x > -tubeWidth);
-
-          if(incrementScore) {
-            setScore((prevScore) => prevScore + 1); 
-          }
+        if (incrementScore) {
+          setScore((prevScore) => prevScore + 1);
+        }
 
         if (newTubes.length === 0 || window.innerWidth - newTubes[newTubes.length - 1].x >= tubeGap) {
           newTubes.push(generateRandomTubePosition());
@@ -125,10 +126,12 @@ function App() {
 
         return newTubes;
       });
+
       detectBaseCollision();
+
       const birdRect = document.querySelector(".bird").getBoundingClientRect();
-      const birdHeight = 50; // Adjust based on your bird image's height
-      const tubeGapHeight = tubeGap - tubeHeight * 2; 
+      const birdHeight = 50;
+      const tubeGapHeight = tubeGap - tubeHeight * 2;
       tubes.forEach((tube) => {
         const upperTubeRect = document.querySelector(".tube-upper").getBoundingClientRect();
         const lowerTubeRect = document.querySelector(".tube-lower").getBoundingClientRect();
@@ -141,22 +144,26 @@ function App() {
           cancelAnimationFrame(animateRef.current);
         }
       });
-        // Floor Collision Detection (Updated Calculation)
-       const baseHeight = 50; // Adjust based on your base image's height
-       const birdBottomPosition = birdPosition + birdHeight - 1; // Adjusted calculation 
-       if (birdBottomPosition >= window.innerHeight - baseHeight) {
-           setGameOver(true);
-           cancelAnimationFrame(animateRef.current);
-       }
+
+      const baseHeight = 50;
+      const birdBottomPosition = birdPosition + birdHeight - 1;
+      if (birdBottomPosition >= window.innerHeight - baseHeight) {
+        setGameOver(true);
+        cancelAnimationFrame(animateRef.current);
+      }
+
       animateRef.current = requestAnimationFrame(animate);
     };
+
     if (gameStarted && !gamePaused && !gameOver) {
       animateRef.current = requestAnimationFrame(animate);
     }
+
     return () => {
       cancelAnimationFrame(animateRef.current);
     };
   }, [gameStarted, gamePaused, birdVelocity, tubes, gameOver]);
+
   const restartGame = () => {
     setBasePosition(0);
     setBirdPosition(window.innerHeight / 2);
@@ -164,15 +171,17 @@ function App() {
     setGameStarted(false);
     setGamePaused(false);
     setGameOver(false);
+    setScore(0);
     setTubes([]);
   };
-  const audioRef = useRef(null); 
+
   useEffect(() => {
-    if(audioRef.current){
-      audioRef.current.src = song; 
-      audioRef.current.load(); 
+    if (audioRef.current) {
+      audioRef.current.src = song;
+      audioRef.current.load();
     }
   }, []);
+
   return (
     <div className="App">
       <div className="overlay" style={{ display: gamePaused ? 'block' : 'none' }} />
